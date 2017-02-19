28d8b48a0b728187e10b1eec7175b0c1d88551b6
Cc <vaal.just@qq.com>
2017-02-19 15:28:45 +0800

    add no238

--- src/com/leetcode/no238/Solution.java
+++ src/com/leetcode/no238/Solution.java
@@ -2,16 +2,20 @@ package com.leetcode.no238;
 
 public class Solution {
 	public int[] productExceptSelf(int[] nums) {
-		int len = nums.length;
-		int[] output = new int[len];
-		int temp = 1;
-		output[0] = 1;
-		for(int i = 1; i < len; i ++) {
-			output[i] = output[i - 1] * nums[i - 1];
+		int[] output = new int[nums.length];
+		for(int i = 1; i < nums.length; i ++) {
+			if(i == 1)
+				output[i] = nums[i - 1];
+			else
+				output[i] = nums[i - 1] * output[i - 1];
 		}
-		for(int i = len - 1; i >= 0; i --) {
-			output[i] = temp * output[i];
-			temp *= nums[i];
+		for(int i = nums.length - 2; i >= 0; i --) {
+			if(i == 0)
+				output[i] = nums[i + 1];
+			else {
+				output[i] *= nums[i + 1];
+				nums[i] *= nums[i + 1];
+			}
 		}
 		return output;
 	}
