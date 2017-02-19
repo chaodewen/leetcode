812a075512634541054c3132aa45c60e7d6d243f
Cc <vaal.just@qq.com>
2017-02-19 14:45:34 +0800

    edit no283

--- src/com/leetcode/no283/Solution.java
+++ src/com/leetcode/no283/Solution.java
@@ -1,20 +1,51 @@
 package com.leetcode.no283;
 
 public class Solution {
+//	public void moveZeroes(int[] nums) {
+//		int zeroLen = 0;
+//		int temp;
+//		for(int i = 0; i < nums.length; i ++) {
+//			if(nums[i] == 0) {
+//				zeroLen ++;
+//			}
+//			else {
+//				temp = nums[i];
+//				nums[i] = 0;
+//				nums[i - zeroLen] = temp;
+//			}
+//		}
+//	}
+
 	public void moveZeroes(int[] nums) {
-		int zeroLen = 0;
-		int temp;
-		for(int i = 0; i < nums.length; i ++) {
+		int idxZero = -1, idxNZero = -1;
+		for(int i = 0; i < nums.length; i ++)
 			if(nums[i] == 0) {
-				zeroLen ++;
+				idxZero = i;
+				break;
+			}
+		for(int i = 0; i < nums.length; i ++)
+			if(nums[i] != 0) {
+				idxNZero = i;
+				break;
+			}
+		if(idxZero == -1 || idxNZero == -1)
+			return;
+		while(idxNZero < nums.length && idxZero < nums.length) {
+			if(nums[idxZero] != 0) {
+				idxZero ++;
+			}
+			else if(nums[idxNZero] == 0 || idxNZero <= idxZero) {
+				idxNZero ++;
 			}
 			else {
-				temp = nums[i];
-				nums[i] = 0;
-				nums[i - zeroLen] = temp;
+				nums[idxZero] = nums[idxNZero];
+				nums[idxNZero] = 0;
+				idxZero ++;
+				idxNZero ++;
 			}
 		}
 	}
+
 	public static void main(String[] args) {
 //		int[] nums = {0, 1, 0, 0, 3, 12};
 		int[] nums = {0};
