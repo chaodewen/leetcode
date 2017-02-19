97b161398ecfa45bae41c0e308a44e6a7b281d67
chaodewen <vaal.just@qq.com>
2017-02-19 17:01:11 +0800

    edit no169

--- src/com/leetcode/no169/Solution.java
+++ src/com/leetcode/no169/Solution.java
@@ -3,21 +3,38 @@ package com.leetcode.no169;
 public class Solution {
 	// Moore Voting Algorithm
 	// 遍历数组，出现不同的将两数删除，相同则继续，最终剩下的就是结果
+//	public int majorityElement(int[] nums) {
+//		int cur = -1;
+//		int cnt = 0;
+//		for(int num : nums) {
+//			if(cnt == 0) {
+//				cur = num;
+//				cnt ++;
+//			}
+//			else if(cur == num) {
+//				cnt ++;
+//			}
+//			else {
+//				cnt --;
+//			}
+//		}
+//		return cur;
+//	}
+
 	public int majorityElement(int[] nums) {
-		int cur = -1;
-		int cnt = 0;
-		for(int num : nums) {
-			if(cnt == 0) {
-				cur = num;
-				cnt ++;
-			}
-			else if(cur == num) {
-				cnt ++;
+		int num = nums[0], cnt = 1;
+		for(int i = 1; i < nums.length; i ++) {
+			if(cnt != 0) {
+				if(num == nums[i])
+					cnt ++;
+				else
+					cnt --;
 			}
 			else {
-				cnt --;
+				num = nums[i];
+				cnt ++;
 			}
 		}
-		return cur;
+		return num;
 	}
 }
