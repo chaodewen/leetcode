8c984c628b2f2d81ee55ecb47eadb3b468533504
Cc <vaal.just@qq.com>
2017-02-28 14:32:48 +0800

    edit no59

--- src/com/leetcode/no59/Solution.java
+++ src/com/leetcode/no59/Solution.java
@@ -2,40 +2,39 @@ package com.leetcode.no59;
 
 public class Solution {
 	public int[][] generateMatrix(int n) {
-		int[][] ret = new int[n][n];
-		if(n == 0) {
-			return ret;
-		}
-		else {
-			int num = 1;
-			for(int i = 0; i < n / 2; i ++) {
-				for(int j = i; j < n - 1 - i; j ++) {
-					ret[i][j] = num ++;
-				}
-				for(int j = i; j < n - 1 - i; j ++) {
-					ret[j][n - i - 1] = num ++;
-				}
-				for(int j = i; j < n - 1 - i; j ++) {
-					ret[n - 1 - i][n - 1 - j] = num ++;
-				}
-				for(int j = i; j < n - 1- i; j ++) {
-					ret[n - 1 - j][i] = num ++;
-				}
+		int[][] matrix = new int[n][n];
+
+		if(n == 0)
+			return matrix;
+
+		int left = 0, right = n - 1, up = 0, down = n - 1, val = 1;
+
+		while(left <= right && up <= down) {
+			for(int i = left; i <= right; i ++)
+				matrix[up][i] = val ++;
+
+			up ++;
+
+			for(int i = up; i <= down; i ++)
+				matrix[i][right] = val ++;
+
+			right --;
+
+			if(up <= down) {
+				for(int i = right; i >= left; i --)
+					matrix[down][i] = val ++;
+
+				down --;
 			}
-			if(ret[n / 2][n / 2] == 0) {
-				ret[n / 2][n / 2] = num;
+
+			if(left <= right) {
+				for(int i = down; i >= up; i --)
+					matrix[i][left] = val ++;
+
+				left ++;
 			}
-			return ret;
 		}
-	}
-	public static void main(String[] args) {
-		int n = 4;
-		int[][] ret = new Solution().generateMatrix(n);
-		for(int i = 0; i < n; i ++) {
-			for(int j = 0; j < n; j ++) {
-				System.out.print(ret[i][j] + " ");
-			}
-			System.out.println();
-		}
+
+		return matrix;
 	}
 }
