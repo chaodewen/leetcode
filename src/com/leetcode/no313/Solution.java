e22e6058967e25d60796ae861bb64550dcb9d0a3
Cc <vaal.just@qq.com>
2017-04-05 01:11:42 +0800

    add no313

--- src/com/leetcode/no313/Solution.java
+++ src/com/leetcode/no313/Solution.java
@@ -1,51 +1,31 @@
 package com.leetcode.no313;
 
-// 题目修改后已超时
 public class Solution {
-	int len;
-	int[] primesCopy;
-	private int findNext(int now, int limit) {
-		int ret = Integer.MAX_VALUE, temp, i = 0;
-		if(primesCopy[0] == 1) {
-			i ++;
-		}
-		for(; i < len; i ++) {
-			temp = now * primesCopy[i];
-			if(temp <= limit) {
-				temp = findNext(temp, limit);
-				if(temp < ret) {
-					ret = temp;
-				}
-			}
-			else {
-				if(temp < ret) {
-					ret = temp;
-				}
-				break;
-			}
-		}
-		return ret;
-	}
 	public int nthSuperUglyNumber(int n, int[] primes) {
-		if(n == 1) {
-			return 1;
+		int[] ugly = new int[n];
+		ugly[0] = 1;
+
+		// 每个值为对应primes中的值指向下一个要乘的数
+		// 保证这是该数能产生的下一个最小丑数
+		int[] pointer = new int[primes.length];
+
+		for(int i = 1; i < n; i ++) {
+			int min = Integer.MAX_VALUE;
+
+			// 为每个primes中的数生成下一个最小丑数
+			// 比较这些丑数，留下最小值作为下一个丑数
+			for(int j = 0; j < primes.length; j ++)
+				min = Math.min(min, ugly[pointer[j]] * primes[j]);
+
+			ugly[i] = min;
+
+			// 根据留下的最小丑数移动pointer所指的数
+			// 结果的两个因数均为primes中的数时会移动两个pointer
+			for(int j = 0; j < primes.length; j ++)
+				if(min == ugly[pointer[j]] * primes[j])
+					pointer[j] ++;
 		}
-		else {
-			len = primes.length;
-			primesCopy = primes;
-			int cnt = 1, i = 1;
-			while(cnt < n) {
-				i = findNext(1, i);
-				cnt ++;
-				System.out.println("cnt : " + cnt + ", i : " + i);
-			}
-			return i;
-		}
-	}
-	public static void main(String[] args) {
-		int[] primes = new int[]{7,19,29,37,41,47,53,59,61,79,83,89,101,103,109,127,131,137,139,157,167,179,181,199,211,229,233,239,241,251};
-		System.out.println(new Solution().nthSuperUglyNumber(100000, primes));
-//		int[] primes = new int[]{1, 2, 4, 7, 8, 13, 14, 16, 19, 26, 28, 32};
-//		System.out.println(new Solution().nthSuperUglyNumber(12, primes));
+
+		return ugly[n - 1];
 	}
 }
