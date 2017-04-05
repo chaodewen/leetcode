bc434c2f5e2ac1d6f3447f06556a3b73716acb21
Cc <vaal.just@qq.com>
2017-04-06 01:41:33 +0800

    modify no189

--- src/com/leetcode/no189/Solution.java
+++ src/com/leetcode/no189/Solution.java
@@ -5,13 +5,23 @@ package com.leetcode.no189;
  */
 
 public class Solution {
+    // lintcode类似题
+    // public void rotateString(char[] str, int offset) {
+    // 	if(str.length > 0)
+    // 		for(int i = 0; i < offset; i ++) {
+    //      	char last = str[str.length - 1];
+    //      	for(int j = str.length - 1; j > 0; j --)
+    //      		str[j] = str[j - 1];
+    //      	str[0] = last;
+    //      }
+    // }
+
     public void rotate(int[] nums, int k) {
         reverse(nums, 0, nums.length - 1);
         reverse(nums, 0, k % nums.length - 1);
         reverse(nums, k % nums.length, nums.length - 1);
     }
-    private void reverse(int[] nums, int i, int j) {
-        int low = i, high = j;
+    private void reverse(int[] nums, int low, int high) {
         while(low < high) {
             int temp = nums[low];
             nums[low ++] = nums[high];
