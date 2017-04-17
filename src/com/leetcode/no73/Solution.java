654881a9862e1c3cb13e6644562afd056fa55290
Cc <vaal.just@qq.com>
2017-04-18 00:21:10 +0800

    modify no73

--- src/com/leetcode/no73/Solution.java
+++ src/com/leetcode/no73/Solution.java
@@ -4,28 +4,25 @@ import java.util.Arrays;
 
 public class Solution {
 	public void setZeroes(int[][] matrix) {
-		int m = matrix.length, n = matrix[0].length, k = 0;
-		// k是第一行首个0的位置，若第一行均不为0则k为n
-		while(k < n && matrix[0][k] != 0) {
-			k ++;
-		}
-		for(int i = 1; i < m; i ++) {
-			for(int j = 0; j < n; j ++) {
-				if(matrix[i][j] == 0) {
+		if(matrix.length == 0 || matrix[0].length == 0)
+			return;
+
+		int row = matrix.length, col = matrix[0].length, index = 0;
+		// index是第一行首个0的位置，若第一行均不为0则index为n
+		while(index < col && matrix[0][index] != 0)
+			index ++;
+
+		for(int i = 1; i < row; i ++)
+			for(int j = 0; j < col; j ++)
+				if(matrix[i][j] == 0)
 					matrix[0][j] = matrix[i][0] = 0;
-				}
-			}
-		}
-		for(int i = 1; i < m; i ++) {
-			for(int j = n - 1; j >= 0; j --) {
-				if(matrix[0][j] == 0 || matrix[i][0] == 0) {
+
+		for(int i = 1; i < row; i ++)
+			for(int j = col - 1; j >= 0; j --) // 必须从右向左，防止因为matrix[0][j]修改最左边的数为0
+				if(matrix[i][0] == 0 || matrix[0][j] == 0)
 					matrix[i][j] = 0;
-				}
-			}
-		}
-		// 说明第一行最初有0，需要全填0
-		if(k < n) {
+
+		if(index < col)
 			Arrays.fill(matrix[0], 0);
-		}
 	}
 }
