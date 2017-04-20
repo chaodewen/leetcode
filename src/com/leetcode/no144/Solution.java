c81a3fae606071e9f10cc0888a722dd67ace7ad0
Cc <vaal.just@qq.com>
2017-04-20 22:32:25 +0800

    add no144

--- src/com/leetcode/no144/Solution.java
+++ src/com/leetcode/no144/Solution.java
@@ -1,34 +1,30 @@
 package com.leetcode.no144;
 
-import java.util.LinkedList;
-import java.util.List;
+import com.leetcode.TreeNode;
 
-class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
-	TreeNode(int x) {
-		val = x;
-	}
-}
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
 
 public class Solution {
-	private List<Integer> list;
 	public List<Integer> preorderTraversal(TreeNode root) {
-		list = new LinkedList<Integer>();
-		if(root == null) {
-			return list;
+		ArrayList<Integer> ans = new ArrayList<>();
+
+		if(root == null)
+			return ans;
+
+		Stack<TreeNode> stack = new Stack<>();
+		stack.push(root);
+
+		while(!stack.isEmpty()) {
+			TreeNode cur = stack.pop();
+			ans.add(cur.val);
+			if(cur.right != null)
+				stack.push(cur.right);
+			if(cur.left != null)
+				stack.push(cur.left);
 		}
-		else {
-			makePreorder(root);
-			return list;
-		}
-	}
-	private void makePreorder(TreeNode node) {
-		if(node != null) {
-			list.add(node.val);
-			makePreorder(node.left);
-			makePreorder(node.right);
-		}
+
+		return ans;
 	}
 }
