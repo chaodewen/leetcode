f1e74300b582684d158fcad5d1ceef3f35e8c07a
Cc <vaal.just@qq.com>
2017-04-26 00:32:52 +0800

    modify no33

--- src/com/leetcode/no33/Solution.java
+++ src/com/leetcode/no33/Solution.java
@@ -2,29 +2,24 @@ package com.leetcode.no33;
 
 public class Solution {
 	public int search(int[] nums, int target) {
-		int len = nums.length, low = 0, high = len - 1;
+		int low = 0, high = nums.length - 1;
 		while(low <= high) {
-			int pos = (low + high) / 2;
-			if(nums[pos] == target)
-				return pos;
-			else if(nums[pos] >= nums[low]) {
-				if(target >= nums[low] && target < nums[pos])
-					high = pos - 1;
+			int mid = (high - low) / 2 + low;
+			if(nums[mid] == target)
+				return mid;
+			else if(nums[mid] >= nums[0]) {
+				if(nums[mid] > target && target >= nums[0])
+					high = mid - 1;
 				else
-					low = pos + 1;
+					low = mid + 1;
 			}
 			else {
-				if(target <= nums[high] && target > nums[pos])
-					low = pos + 1;
+				if(nums[mid] < target && target <= nums[nums.length - 1])
+					low = mid + 1;
 				else
-					high = pos - 1;
+					high = mid - 1;
 			}
 		}
 		return -1;
-	}
-	public static void main(String[] args) {
-		Solution s = new Solution();
-		int[] nums = new int[] { 4, 5, 5, 5, 6, 7, 7, 0, 1, 2, 2, 2 };
-		System.out.println(s.search(nums, 7));
 	}
 }
