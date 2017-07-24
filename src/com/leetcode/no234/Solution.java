b6fbe4e0fa19a20534ebef2b608068418245e1fe
Cc <vaal.just@qq.com>
2017-07-24 21:44:06 +0800

    modified 234

--- src/com/leetcode/no234/Solution.java
+++ src/com/leetcode/no234/Solution.java
@@ -3,38 +3,40 @@ package com.leetcode.no234;
 import com.leetcode.ListNode;
 
 /**
- * Created by Cc on 16/10/6.
+ * Created by Cc on 2017/7/17.
  */
-
 public class Solution {
     public boolean isPalindrome(ListNode head) {
-        if(head == null)
-            return true;
+        if(head == null) return true;
+
         ListNode slow = head, fast = head;
-        while(fast != null && fast.next != null) {
+        while(fast.next != null && fast.next.next != null) {
+            slow = slow.next;
             fast = fast.next.next;
+        }
+
+        // slow是中点，rightMid存中点（奇数个节点）或右中点（偶数）
+        ListNode rightMid = fast.next == null ? slow : slow.next;
+
+        // 反向
+        fast = slow.next;
+        while(fast != null) {
+            ListNode temp = fast.next;
+            fast.next = slow;
+            slow = fast;
+            fast = temp;
+        }
+
+        // slow是末尾节点且后半部分已经反向
+
+        while(head != rightMid) {
+            if(head.val != slow.val)
+                return false;
+
+            head = head.next;
             slow = slow.next;
         }
-        ListNode left = head, right = reverse(slow);
-        while(left != slow) {
-            if(left.val != right.val)
-                return false;
-            left = left.next;
-            right = right.next;
-        }
+
         return true;
-    }
-    private ListNode reverse(ListNode head) {
-        if(head == null)
-            return head;
-        ListNode p1 = head, p2 = head.next, temp;
-        while(p2 != null) {
-            temp = p2;
-            p2 = p2.next;
-            temp.next = p1;
-            p1 = temp;
-        }
-        head.next = null;
-        return p1;
     }
 }
