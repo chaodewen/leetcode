78b12808e4335d64480ad40b9814255dfcb183c2
Cc <vaal.just@qq.com>
2017-07-24 22:37:41 +0800

    modified no70

--- src/com/leetcode/no70/Solution.java
+++ src/com/leetcode/no70/Solution.java
@@ -1,21 +1,15 @@
 package com.leetcode.no70;
 
 public class Solution {
-	public int climbStairs(int n) {
-		if(n == 0 || n == 1) {
-			return n;
-		}
-		else if(n == 2) {
-			return 2;
-		}
-		else {
-			int[] dp = new int[n + 1];
-			dp[1] = 1;
-			dp[2] = 2;
-			for(int i = 3; i <= n; i ++) {
-				dp[i] = dp[i - 1] + dp[i - 2];
-			}
-			return dp[n];
-		}
-	}
+    public int climbStairs(int n) {
+        int last = 1, ans = 1;
+
+        for(int i = 2; i <= n; i ++) {
+            int temp = ans;
+            ans += last;
+            last = temp;
+        }
+
+        return ans;
+    }
 }
